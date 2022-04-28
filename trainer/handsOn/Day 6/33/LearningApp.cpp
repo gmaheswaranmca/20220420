4b86370ca654ef21fe1d1cdf2726f15bc1ac9f59
Maheswaran <pystud19@gmail.com>
2022-04-28 15:17:31 +0530

    Pointers along with const qualifiers

--- trainer/handsOn/Day 6/33/LearningApp.cpp
+++ trainer/handsOn/Day 6/33/LearningApp.cpp
@@ -2,24 +2,25 @@
   Read two numbers and find sum using pointers 
 Notes: Declare variables "firstNum", "secondNum", "sum" 
 and pointers "firstNumAddr", "secondNumAddr", "sumAddr" 
-to point them respectively. 
+to point them respectively.
+ 
 Migrate into functions
 
-const qualifier for data
+const qualifier for pointer
 */
 #include<iostream>
 using namespace std;
-void inputNumbers(int* firstNumAddr, int* secondNumAddr) {
+void inputNumbers(int*const firstNumAddr, int* const secondNumAddr) {
 	cout << "Enter first number:";
-	cin >> (*firstNumAddr); // (*firstNumAddr) alias for firstNum 
+	cin >> (*firstNumAddr); 
 
 	cout << "Enter second number:";
 	cin >> (*secondNumAddr);
 }
-void doFindSum(const int* firstNumAddr, const int* secondNumAddr, int* sumAddr) {
+void doFindSum(const int*const firstNumAddr, const int* const secondNumAddr, int* const sumAddr) {
 	(*sumAddr) = (*firstNumAddr) + (*secondNumAddr);
 }
-void print(const int* firstNumAddr, const int* secondNumAddr, const int* sumAddr) {
+void print(const int* const firstNumAddr, const int* const secondNumAddr, const int* const sumAddr) {
 	cout << "First number:" << (*firstNumAddr) << endl;
 	cout << "Second number:" << (*secondNumAddr) << endl;
 	cout << "Sum:" << (*sumAddr) << endl;
