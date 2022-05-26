a804d80bfd80100fdd6a54c6cf925edc06c9a59b
Maheswaran <pystud19@gmail.com>
2022-05-26 14:00:18 +0530

    NOTES

--- trainer/handsOn/Day 24/sortSubLists-02.cpp
+++ trainer/handsOn/Day 24/sortSubLists-02.cpp
@@ -1,22 +1,6 @@
 /*
-Case 1
-input:   N=9, 
-         numbers= 3 7 8 5 2 1 9 5 4
-		          -----
-				        -----
-						      ------
-output:           3 7 8 1 2 5 4 5 9
-Case 2
-input : N=8,
-		numbers = 6 5 3 1 8 7 2 4 
-				  ----- 
-				        -----
-						      ---
-output:           3 5 6 1 7 8 2 4 
-
-
 input[N=9,numbers= 3 7 8 5 2 1 9 5 4]	
-output[3 7 8 1 2 5 4 5 9]
+output[3 7 8 1 2 5 4 5 9 ]
 
 input[N=8,numbers= 6 5 3 1 8 7 2 4]	
 output[3 5 6 1 7 8 2 4]
@@ -57,30 +41,9 @@ void printArray(int arr[], int size)
     cout << endl;
 }
 
-void sortSublist(int arr[],int N){
-     bubbleSortv3(arr, 0, 2);
-   bubbleSortv3(arr, 3, N-6);
-   bubbleSortv3(arr, N-5, N-1);
-}
 
-void sortSublistv2(int arr[],int N){
-     int startIndex,endIndex,sublistSize=3;
-     startIndex =0;
-     endIndex =startIndex+sublistSize -1;
-     
-     bubbleSortv3(arr,startIndex , endIndex);
-     startIndex =endIndex+1;
-     endIndex =startIndex+sublistSize -1;
-      bubbleSortv3(arr,startIndex , endIndex);
-  
-  startIndex =endIndex+1;
-     endIndex =startIndex+sublistSize -1;
-      bubbleSortv3(arr,startIndex , endIndex);
-  
-}
-
-void sortSublistv3(int arr[],int N){
-     int startIndex,endIndex,sublistSize=3;
+void sortSublistv3(int arr[],int N,int sublistSize){
+     int startIndex,endIndex;
      int SubListCount=N/sublistSize;        //11/3 =3       12/3=4      15/3=5
      if(N%sublistSize!=0)           //11%3 =2       12%3==0     15%3==0
      {
@@ -108,7 +71,8 @@ int main()
    
     int arr[] ={6, 5, 3, 1, 8, 7, 4, 2, 0, 30, 20};      
     int N = sizeof(arr) / sizeof(arr[0]);
-    int subListSize = 3;
+    //int subListSize = 3;
+    int subListSize = 4;
     
     cout << "before sorting array: \n";
     printArray(arr, N);
