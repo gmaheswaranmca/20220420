ae1326c919bf5d778574a50ab747a79548521335
Maheswaran <pystud19@gmail.com>
2022-05-27 15:27:34 +0530

    NOTES

--- trainer/handsOn/Day 25/Qn20220527-10.cpp
+++ trainer/handsOn/Day 25/Qn20220527-10.cpp
@@ -1,15 +1,21 @@
 Printing Number pattern
 Get the integer value from user and print the following pattern
-If the input is not positive and not between 1 and 10 print Not possible
+If the input is not positive and 
+                not between 1 and 10 
+Print Not possible
 
 Input:
  	5
 Output:
-15
-14	13
-12	11	10
-9	8	7	6
-5	4	3	2	1
+15                          1
+14	13                      2
+12	11	10                  3
+9	8	7	6               4
+5	4	3	2	1           5=15   1+2+3+...+N = N*(N+1)/2   for 5 = 5*(6/2)=15
+                                   for I -> 1 to N 
+                                        sum of I    
+                                        
+                                   patterns start at sumOfI
 
 
 Test Case 4:
@@ -29,3 +35,61 @@ Output:
 6
 5	4
 3	2	1
+
+
+Test Case 3:
+Input:
+11
+Output:
+Not Possible
+
+Test Case 4:
+Input:
+-5
+Output:
+Not Possible
+
+
+
+
+
+*
+**
+***
+****
+*****
+
+*****    
+ ****
+  ***
+   **
+    *
+
+    *
+   **
+  ***
+ ****
+*****
+
+    *
+   ***
+  *****
+ *******
+*********
+
+
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+    
+    
+    
+
+
+
