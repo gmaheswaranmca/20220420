075450af2edade6bb2414081e87777ed363ebb7c
Maheswaran <pystud19@gmail.com>
2022-05-27 14:15:12 +0530

    NOTES

---
+++ trainer/handsOn/Day 25/Qn20220527-08.cpp
@@ -0,0 +1,64 @@
+/*
+Write the range function 
+to print prime numbers between r1(start number) and r2(end number)
+NOTE: Use Pointers in the range function for the two argumetns 
+
+Input:
+10  20
+Output:
+11	13	17	19
+
+Input:
+-12   30
+Output:
+-1
+
+Test Case 1:
+Input:
+5  17
+Output:
+5 7 11 13 17
+Test Case 2:
+Input:
+2  25
+Output:
+2 3 5 7 11 13 17 19 23	
+
+Test Case 3:
+Input:
+10  30
+Output:
+11 13 17 19 23 29
+
+Test Case 4:
+Input:
+10  -4
+Output:
+-1
+
+Test Case 5:
+Input:
+7  7
+Output:
+7
+
+
+Initial Code:
+#include<iostream>
+using namespace std;
+bool isPrimeNumber(int num){
+	return false;
+}
+void range(int* startAddr, int* endAddr) {
+    //Print Prime Numbers between
+}
+void main()
+{
+    int start, end;
+    cout << "Enter start range:";
+    cin >> start;
+    cout << "Enter end range:";
+    cin >> end;
+    range(&start, &end);
+}
+ */
