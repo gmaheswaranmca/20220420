18949afb639fe9a6ec2acd2b9e8e0aeaed8fdb0d
Maheswaran <pystud19@gmail.com>
2022-05-27 10:59:18 +0530

    NOTES

--- trainer/handsOn/Day 25/Qn20220527-05.cpp
+++ trainer/handsOn/Day 25/Qn20220527-05.cpp
@@ -1,3 +1,4 @@
+/*
 Print "perfect squares" from given numbers: 
 Get list of numbers into an array and find and print the perfect squares.
 It we cannot find one perfect square, then print just -1.
@@ -49,3 +50,45 @@ Input:
 256  196  8   25
 Output:
 256	196	25
+*/
+#include<iostream>
+using namespace std;
+
+void read(int numbers[],int size){
+    cout << "Enter numbers one by one:" << endl;
+    for(int I=0;I<size;I++){
+        cout << "Enter number at " << I << ":"; cin >> numbers[I];
+    }
+}
+bool isPerfectSquare(int num){
+    //Write your code
+    return false;
+}
+void printPerfect(int numbers[],int size){
+    int count = 0;
+    cout << "Perfect squares are:" << endl;
+    for(int I=0;I<size;I++){
+        if(isPerfectSquare(numbers[I]){
+            cout << numbers[I] << " ";
+            count++;
+        }
+    }
+    if(count==0){
+        cout << -1;
+    }
+}
+int main(){
+    int numbers[] = {25,  30,  40,  56,  10,  4,  81,  0};
+    //int numbers[] = {10,  20,  30,  40,  50,  60};
+    
+    int N = sizeof(numbers)/sizeof(int);
+    //cout << "Enter size of numbers:"; cin >> N;
+    //read(numbers,N);
+    
+    printPerfect(numbers,N);
+    
+    return 0;
+}
+
+
+
